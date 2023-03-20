d7e07788391d43e6bde8743492d5fae9b29c5aa8
Ana <ana_co_soares@estudante.sesisenai.org.br>
2023-03-20 09:45:34 -0300

    uns bglh ai de arrow function

--- java.js
+++ java.js
@@ -1,24 +1,36 @@
 let res = document.getElementById('res')
 
-let pais = ['chile', 'bolivia', 'brasil', 'argentina', 'paraguai']
-let cidades = new Array('tijucas', 'sao jose', 'canelinha', 'itapema', 'nova trento')
+let pais = [' chile ', ' bolivia ', ' brasil ', ' argentina ', ' paraguai ']
+let cidades = new Array(' tijucas', ' sao jose', ' canelinha', ' itapema', ' nova trento')
 
-pais.push('uruguai')
-pais.unshift('colombia')
-console.log(pais)
-console.log(cidades)
+// pais.push('uruguai')
+// pais.unshift('colombia')
+// console.log(pais)
+// console.log(cidades)
 
-pais.pop()
-pais.shift()
-console.log(pais)
+// pais.pop()
+// pais.shift()
+// console.log(pais)
 
+// console.log(pais.indexOf('brasil'))
 
-function inserir(){
+let nome_pais = document.getElementById('nome_pais')
+let incluir = document.getElementById('incluir')
 
-    pais.pop()
-    pais.shift()
-
-    res.innerHTML += pais + '<br>' + '<br>'
+incluir.addEventListener('click', ()=>{
+    res.innerHTML += pais + '<br>'
     res.innerHTML += cidades + '<br>'
+})
 
-}
+// function inserir(){
+
+//     pais.splice(3,0, " estados unidos", " deus")
+//     pais.splice(4,1,)
+
+//     // pais.pop()
+//     // pais.shift()
+
+//     res.innerHTML += pais + '<br>' + '<br>'
+//     res.innerHTML += cidades + '<br>'
+
+// }
